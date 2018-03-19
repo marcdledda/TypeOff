b2ef651b9a510f0390a5ba25a09169c127ccce93
marcdledda <marcdl636@gmail.com>
2018-03-19 11:08:59 -0500

    changed index.html, main.js, main.scss; phaser issues

---
+++ js/main.js
@@ -0,0 +1,23 @@
+"use strict";
+
+require("../phaser.min.js");
+
+console.log("main.js test");
+
+var game = new Phaser.Game(854, 480, Phaser.CANVAS, null, {
+    preload: preload, create: create, update: update
+});
+
+function preload(){
+    // game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
+    // game.scale.pageAlignHorizontally = true;
+    // game.scale.pageAlignVertically = true;
+}
+
+function create(){
+
+}
+
+function update(){
+
+}
