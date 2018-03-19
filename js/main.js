681a73af9efedfbf65bec3d674abbc49a3d5dd7e
marcdledda <marcdl636@gmail.com>
2018-03-19 18:53:03 -0500

    all states are within main.js

--- js/main.js
+++ js/main.js
@@ -5,22 +5,49 @@ let Phaser = require("../phaser.min.js");
 let mainMenu = require("./mainMenu.js");
 
 let titleIMG;
+let menuIMG;
 
 var game = new Phaser.Game(854, 480, Phaser.CANVAS, null);
 
-let titleState = {
+let loadState = {
     preload: function(){
         game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
         game.scale.pageAlignHorizontally = true;
         game.scale.pageAlignVertically = true;
         game.load.image('titleIMG', 'js/media/ttl-scrn.png');
+        game.load.image('menuIMG', 'js/media/mainMenu.png');
     },
 
     create: function(){
-        titleIMG = game.add.image(game.world.width*0.5, game.world.height*0.5, 'titleIMG');
-        titleIMG.anchor.set(0.5);
+        game.state.start('title');
     }
 };
 
+let titleState = {
+    create: function(){
+        titleIMG = game.add.image(game.world.width*0.5, game.world.height*0.5, 'titleIMG');
+        titleIMG.anchor.set(0.5);
+
+        game.input.onDown.add(this.change, this);
+    },
+
+    change: function(){
+        game.state.start('menu');
+    }
+};
+
+let menuState = {
+    create: function(){
+        menuIMG = game.add.image(game.world.width*0.5, game.world.height*0.5, 'menuIMG');
+        menuIMG.anchor.set(0.5, 0.5);
+    }
+};
+
+// function changeScreen(){
+//     console.log("click!");
+// }
+
+game.state.add('load', loadState);
 game.state.add('title', titleState);
-game.state.start('title');
+game.state.add('menu', menuState);
+game.state.start('load');
--- js/mainMenu.js
+++ js/mainMenu.js
@@ -1,5 +1,17 @@
 "use strict";
 console.log("mainMenu.js test");
 
-let Phaser = require("../phaser.min.js");
-let main = require("./main.js");
+// let Phaser = require("../phaser.min.js");
+// let main = require("./main.js");
+
+// // let menuIMG;
+// let game = main.game;
+
+// let menuState = {
+//     create: function(){
+//         let menuIMG = game.add.image(game.world.width*0.5, game.world.height*0.5, 'menuIMG');
+//         menuIMG.anchor.set(0.5, 0.5);
+//     }
+// };
+
+// module.exports = {menuState};
